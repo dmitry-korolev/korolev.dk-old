bffab564a7b98058b8e88c95cf524c0ed0561240
Dmitry Korolev <dmitry@korolev.dk>
2017-03-25 21:03:47 +0100

    Revert "Remove karma testing"
    This reverts commit 33ba64ab8a5776fdb00704a89af39dbd7e2519a3.

--- config/main.js
+++ config/main.js
@@ -3,7 +3,8 @@
 var config = {
     env: process.env.NODE_ENV || 'development',
     host: process.env.HOST || 'localhost',
-    port: process.env.PORT || 8889
+    port: process.env.PORT || 8889,
+    karmaPort: 9876
 };
 
 module.exports = config;
