33ba64ab8a5776fdb00704a89af39dbd7e2519a3
Dmitry Korolev <dk@mustdigital.ru>
2017-03-25 20:40:54 +0300

    Remove karma testing

--- config/main.js
+++ config/main.js
@@ -3,8 +3,7 @@
 var config = {
     env: process.env.NODE_ENV || 'development',
     host: process.env.HOST || 'localhost',
-    port: process.env.PORT || 8889,
-    karmaPort: 9876
+    port: process.env.PORT || 8889
 };
 
 module.exports = config;
