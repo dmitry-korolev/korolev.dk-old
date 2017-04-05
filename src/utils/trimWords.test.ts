1950437aab6626787d04c89410d60d4ac58ca120
Dmitry Korolev <dk@mustdigital.ru>
2017-04-05 19:14:01 +0300

    Remove trimwords

--- src/utils/trimWords.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { trimWords } from './trimWords';
-
-xdescribe('trimWords', () => {
-    it('should trim sentence by words', () => {
-        expect(trimWords('yretwqs drwqreqs erhyer etygfer', 2)).to.equal('yretwqs');
-    });
-});
--- src/utils/trimWords.ts
+++
@@ -1,1 +0,0 @@
-export const trimWords = (source: string, letters: number): string => source + letters;
